47eb5e19c1911decd53d82615194559d38bd3599
nam.dinh <dragonsang10a1@gmail.com>
2023-05-19 16:54:43 +0700

    implement controller for getting the list of email from input email,

---
+++ src/main/java/org/FriendsManagement/controller/CustomerReactiveController.java
@@ -0,0 +1,25 @@
+package org.FriendsManagement.controller;
+
+import org.FriendsManagement.service.FriendShipReactiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CustomerReactiveController {
+
+    private final FriendShipReactiveService friendShipReactiveService;
+
+    @Autowired
+    public CustomerReactiveController(FriendShipReactiveService friendShipReactiveService) {
+        this.friendShipReactiveService = friendShipReactiveService;
+    }
+
+    @PostMapping(value = "/users/friends")
+    public Flux<String> getFriendList(@RequestBody Mono<String> email) {
+        return friendShipReactiveService.getFriendsListByEmail(email);
+    }
+}
--- src/main/java/org/FriendsManagement/controller/UserStreamController.java
+++ src/main/java/org/FriendsManagement/controller/UserStreamController.java
@@ -2,7 +2,7 @@ package org.FriendsManagement.controller;
 
 import org.FriendsManagement.model.ResponseObjectTest;
 import org.FriendsManagement.model.User;
-import org.FriendsManagement.service.UserService;
+import org.FriendsManagement.service.UserStreamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +11,11 @@ import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 
-import static org.FriendsManagement.controller.UserController.USER_ROUTING;
+import static org.FriendsManagement.controller.UserStreamController.USER_ROUTING;
 
 @RestController
 @RequestMapping(path = USER_ROUTING)
-public class UserController {
+public class UserStreamController {
     protected static final String USER_ROUTING = "/api/v1/users";
     protected static final String GET_ALL_USERS = "";
     protected static final String SAVE_USER = "/";
@@ -23,27 +23,27 @@ public class UserController {
     protected static final String UPDATE_USER_BY_ID = "/{userID}";
     protected static final String DELETE_USER_BY_ID = "/{userId}";
 
-    private final UserService userService;
+    private final UserStreamService userStreamService;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserStreamController(UserStreamService userStreamService) {
+        this.userStreamService = userStreamService;
     }
 
 
     @GetMapping(value = GET_ALL_USERS)
     public Flux<User> getAllUsers() {
-        return userService.getAllUsers();
+        return userStreamService.getAllUsers();
     }
 
     @PostMapping(value = SAVE_USER, consumes = "application/json")
     public Mono<User> saveUser(@RequestBody User user) {
-        return userService.createUser(user);
+        return userStreamService.createUser(user);
     }
 
     @GetMapping(GET_USER_BY_ID)
     public Mono<ResponseEntity<ResponseObjectTest>> findUserById(@PathVariable String userId) {
-        Mono<User> userByID = userService.findById(userId);
+        Mono<User> userByID = userStreamService.findById(userId);
         return userByID.flatMap(
                 user -> {
                     return Mono.just(ResponseEntity.ok().body(
@@ -64,11 +64,11 @@ public class UserController {
 
     @PutMapping(UPDATE_USER_BY_ID)
     public Mono<User> updateUserByUserId(@PathVariable String userId, @RequestBody User user) {
-        return userService.updateUser(userId, user);
+        return userStreamService.updateUser(userId, user);
     }
 
     @DeleteMapping(DELETE_USER_BY_ID)
     public Mono<User> deleteUserByUserId(@PathVariable String userId) {
-        return userService.deleteUser(userId);
+        return userStreamService.deleteUser(userId);
     }
 }
